e6575764946c2ad52783cdd17b2be97f9c9cd1f9
renato colangelo <colangelo95@hotmail.it>
2021-01-09 19:41:55 +0100

    1) Added new stock/ETF -"XTRACKERS MSCI WORLD HEALTH CARE (XDWH.MI)", -"ISHARES ELECTRIC VEHICLES AND DRIVING TEC (ECAR.MI)", -"WISDOMTREE PHYSICAL GOLD (PHAU.MI)", -"ISHARES EURO STOXX (EXSI.DE)", -"ISHARES NASDAQ 100 (CSNDX.MI)", -"ISHARES CORE MSCI WORLD (SWDA.MI)", -"ALIBABA" 2) Display number of observations used in the portfolio simulation 3) Display a stock/portfolio summary containing mean, sd, worst, max, median of the monthly returns 4) Display a new performance chart

--- call_body.R
+++ call_body.R
@@ -5,11 +5,13 @@ call_body <- dashboardBody(
                                 }
 
                                 '))),
-  
+  h6(textOutput("n_obs")),
   plotlyOutput("graph"),
   fluidRow(column(plotlyOutput("graph_2"),width=6),  
-           column(plotlyOutput("graph_3"),width=6)
-  ),
-           fluidRow(plotOutput("graph_4"))
+           column(plotlyOutput("graph_3"),width=6)),
+  fluidRow(column(plotOutput("graph_4")  , width=6),
+           column(DT::dataTableOutput(outputId ="summary"), width = 6)),
+  fluidRow(plotlyOutput("graph_5"))
+  
 )
 
--- call_sidebar.R
+++ call_sidebar.R
@@ -1,5 +1,5 @@
 call_sidebar <- dashboardSidebar(  
-                  tags$head(tags$style(HTML('   /* body */
+   tags$head(tags$style(HTML('   /* body */
                                 .sidebar {
                                 font-size: 10px;
                                 }
@@ -7,22 +7,28 @@ call_sidebar <- dashboardSidebar(
                                 margin-bottom: 2px;
                                 }
                                 '))),
-      checkboxGroupInput(inputId = "USA","USA",
-                         c("AMAZON (AMZN)","BOEING (BA)","FACEBOOK (FB)","GOOGLE (GOOG)",
-                           "MICROSOFT (MSFT)","NRG ENERGY (NRG)","WALMART (WMT)"),
-                     selected = c("FACEBOOK (FB)")
-      ),
-      checkboxGroupInput(inputId = "EU","EU",
-                         c("AXA SA (CS.PA)",
-                           "DALMIER AG (DAI.DE)","ENEL MI","ENI MI","FERRARI NV (RACE)",
-                           "NOKIA (NOK)","TOTAL (TOT)","VODAFONE PLC (VOD)","VOLKSWAGEN AG (WOW.DE)"),
-                         selected = c("FERRARI NV (RACE)")
-      ),
-      checkboxGroupInput(inputId = "ETF","ETF",
-                         c("ISHARE GLOBAL CLEAN ENERGY ETF (ICLN)","ISHARE LARGE CAP ETF (FXI)","KRANESHARES CSI CHINA INTERNET ETF (KWEB)","VANGUARD ITC INDEX ETF (VGT)" ,
-                           "VANGUARD REAL ESTATE INDEX ETF (VNQ)","VANGAURD S&P 500 ETF (VOO)","XTRACKERS HARVEST CSI 300 CHINA A ETF (ASHR)"),
-                         selected = c("ISHARE GLOBAL CLEAN ENERGY ETF (ICLN)")
-   
-      ),
-      actionButton(inputId="BUTTON_1", label="OK")
+   checkboxGroupInput(inputId = "USA","USA",
+                      c("AMAZON (AMZN)","BOEING (BA)","FACEBOOK (FB)","GOOGLE (GOOG)",
+                        "MICROSOFT (MSFT)","NRG ENERGY (NRG)","WALMART (WMT)"),
+                      selected = c("FACEBOOK (FB)")
+   ),
+   checkboxGroupInput(inputId = "EU","EU",
+                      c("AXA SA (CS.PA)",
+                        "DALMIER AG (DAI.DE)","ENEL MI","ENI MI","FERRARI NV (RACE)",
+                        "NOKIA (NOK)","TOTAL (TOT)","VODAFONE PLC (VOD)","VOLKSWAGEN AG (WOW.DE)"),
+                      selected = c("FERRARI NV (RACE)")
+   ),
+   checkboxGroupInput(inputId = "CHINA","CHINA",
+                      c("ALIBABA (BABA)")
+   ),
+   checkboxGroupInput(inputId = "ETF","ETF",
+                      c("ISHARE GLOBAL CLEAN ENERGY ETF (ICLN)","ISHARE LARGE CAP ETF (FXI)","KRANESHARES CSI CHINA INTERNET ETF (KWEB)","VANGUARD ITC INDEX ETF (VGT)" ,
+                        "VANGUARD REAL ESTATE INDEX ETF (VNQ)","VANGAURD S&P 500 ETF (VOO)","XTRACKERS HARVEST CSI 300 CHINA A ETF (ASHR)","XTRACKERS MSCI WORLD HEALTH CARE (XDWH.MI)",
+                        "ISHARES ELECTRIC VEHICLES AND DRIVING TEC (ECAR.MI)", "WISDOMTREE PHYSICAL GOLD (PHAU.MI)", "ISHARES EURO STOXX (EXSI.DE)",
+                        "ISHARES NASDAQ 100 (CSNDX.MI)", "ISHARES CORE MSCI WORLD (SWDA.MI)"),
+                      selected = c("ISHARE GLOBAL CLEAN ENERGY ETF (ICLN)")
+                      
+   ),
+   dateInput(inputId = "input_date", label = "Data fine"),
+   actionButton(inputId="BUTTON_1", label="OK")
 )
